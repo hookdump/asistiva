a6fedca45b94c6e813c2ae8f07c208c013504a8e
Ignacio Freiberg <hookdump@gmail.com>
2018-10-04 01:10:05 -0300

    Create Workspace component #7

---
+++ client/src/app/workspace/workspace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'workspace',
+  templateUrl: './workspace.component.html',
+  styleUrls: ['./workspace.component.sass']
+})
+export class WorkspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
